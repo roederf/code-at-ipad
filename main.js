fb4d3f0c4c3066cd17927d0d9ee98c9266078926
Florian Roeder <roederf@gmx.de>
2020-12-29 12:34:16 +0100

    Ball hinzugefuegt

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
+var context = null;
+
 function Vorbereitung(){
-    console.log("Vorbereitung starten");
+    var canvas = document.getElementById('Spielflaeche');
     
-    var canvas = document.getElementById('Spielflaeche'),
     context = canvas.getContext('2d');
 
-    var gorilla_bild_1 = new Image();
-    gorilla_bild_1.src = 'donkey-kong.png';
-    gorilla_bild_1.onload = function(){
-        context.drawImage(gorilla_bild_1, 100, 300);
+    var gorilla_bild = new Image();
+    gorilla_bild.src = 'donkey-kong.png';
+    gorilla_bild.onload = function(){
+        context.drawImage(gorilla_bild, 100, 300);
+        context.drawImage(gorilla_bild, 900 - 64, 300);
     }
 
-    var gorilla_bild_2 = new Image();
-    gorilla_bild_2.src = 'donkey-kong.png';
-    gorilla_bild_2.onload = function(){
-        context.drawImage(gorilla_bild_2, 900 - 64, 300);
-    }
-
-    console.log("Vorbereitung fertig");
 }
 
 function Abwerfen(){
+    var winkel = document.getElementById('EingabeWinkel').value;
+    var kraft = document.getElementById('EingabeKraft').value;
+
+    context.beginPath();
+    context.arc(50, 50, 10, 0, 2*Math.PI);
+    context.fillStyle="red";
+    context.fill();
+    
     console.log("Es wurde abgworfen");
 }
